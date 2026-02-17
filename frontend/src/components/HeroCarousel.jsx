ea3dce9bfc537dc3718efc5b96f008e1583fe324
Narayandwivedi <desinplus1@gmail.com>
2026-02-17 19:11:11 +0530

    improve ui

--- frontend/src/components/HeroCarousel.jsx
+++ frontend/src/components/HeroCarousel.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const HeroCarousel = () => {
   return (
-    <section className="w-full md:w-1/2 md:mx-auto">
+    <section className="w-full md:w-[55%] lg:w-[90%] md:mx-auto">
       <picture>
-        <source media="(max-width: 767px)" srcSet="/onionmobile.webp" />
+        <source media="(max-width: 767px)" srcSet="/onion3.png" />
         <img
-          src="/oniondesktop.webp"
+          src="/onion3.png"
           alt="ABCD Market Hero Banner"
           className="w-full h-auto block"
         />
